9a7f635e5792a86715f4a139eff884a90e208b5c
pawel <pawel@domena.pl>
2016-03-27 20:51:21 +0200

    [Akka1] Java Example

---
+++ src/main/scala/jug/workshops/reactive/akka/a1/java/ScalaActorCallingJava.scala
@@ -0,0 +1,22 @@
+package jug.workshops.reactive.akka.a1.java
+
+import akka.actor.{Props, ActorSystem}
+
+/**
+  * Created by pawel on 27.03.16.
+  */
+object ScalaActorCallingJava {
+
+  def main(args: Array[String]) {
+    val system=ActorSystem("scala-calling-java")
+
+    val javaActor=system.actorOf(Props[JavaActor])
+
+
+    javaActor ! "hello"
+    javaActor ! "other message"
+
+    system.terminate()
+  }
+
+}
