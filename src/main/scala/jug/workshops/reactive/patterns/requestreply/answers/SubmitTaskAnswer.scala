7946f9face0ecdf978d6dddd8f43454f32dc6b18
pawel <pawel@domena.pl>
2016-10-09 20:11:10 +0200

    [Patterns] RequestReply : delegate node

--- src/main/scala/jug/workshops/reactive/patterns/requestreply/answers/SubmitTaskAnswer.scala
+++ src/main/scala/jug/workshops/reactive/patterns/requestreply/answers/SubmitTaskAnswer.scala
@@ -3,6 +3,7 @@ package jug.workshops.reactive.patterns.requestreply.answers
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import jug.workshops.reactive.patterns.requestreply.answers.DelegateNode.DelegatedTask
 import jug.workshops.reactive.patterns.requestreply.answers.SimpleNode._
 
 /**
@@ -13,12 +14,13 @@ object SubmitTaskAnswer {
   def main(args: Array[String]): Unit = {
     val system=ActorSystem("RequestReply")
 
-    val simpleNode=system.actorOf(Props[SimpleNode],"simpleNode")
-    val client=system.actorOf(Props(new Client(simpleNode)),"client")
+    val simpleNode=system.actorOf(Props[SimpleNode],"simpleNode") //EXERCISE 1
+    val delegateNode=system.actorOf(Props[DelegateNode],"delegateNode")  //EXERCISE 2
+    val client=system.actorOf(Props(new Client(delegateNode)),"client")
 
     client ! "START"
 
-    TimeUnit.MILLISECONDS.sleep(500)
+    TimeUnit.MILLISECONDS.sleep(1000)
     system.terminate()
   }
 
@@ -29,12 +31,18 @@ class Client(server: ActorRef) extends Actor{
     case "START" =>
       server ! SquareRoot(16.0)
       server ! Sum(Seq(1,2,3,4,5))
+      server ! Sum(1.0 to 1000.0 by 1)
+      server ! SquareRoot(25.0)
+      server ! SquareRoot(36.0)
+      server ! SquareRoot(49.0)
+      server ! Sum(1.0 to 10.0 by 1)
 
     case Response(body) =>
       println(s"client received response from server $body")
   }
 }
 
+//EXERCISE 1 - SIMPLE NODE
 
 class SimpleNode extends Actor{
   import SimpleNode._
@@ -52,3 +60,47 @@ object SimpleNode{
 
   case class Response(body:Double)
 }
+
+
+//EXERCISE 2 - DYNAMIC POOL
+
+class DelegateNode extends Actor{
+
+  var childCounter = 0
+
+  override def receive: Receive = {
+    case task : Task =>
+      val child=context.actorOf(Props[PerRequestWorker],generateChildName)
+      child ! DelegatedTask(sender,task)
+      println(s"delegated ${task} to ${child.path}")
+  }
+
+  def generateChildName:String = {
+    val name = s"child$childCounter"
+    childCounter=childCounter+1
+    name
+  }
+}
+
+class PerRequestWorker extends Actor {
+  override def receive: Receive = {
+    case DelegatedTask(replyTo,SquareRoot(number)) =>
+      latency
+      replyTo ! Response(Math.sqrt(number))
+    case DelegatedTask(replyTo,Sum(numbers)) =>
+      latency
+      replyTo ! Response(numbers.sum)
+  }
+
+  def latency={
+    println(s"delegate ${self.path.name} working in thread ${Thread.currentThread().getName}")
+    TimeUnit.MILLISECONDS.sleep(500)
+  }
+}
+
+object DelegateNode {
+  import SimpleNode._
+
+  case class DelegatedTask(replyTo:ActorRef, task:Task)
+}
+
--- src/test/scala/jug/workshops/reactive/patterns/requestreply/answers/RequestReplyAnswerSpec.scala
+++ src/test/scala/jug/workshops/reactive/patterns/requestreply/answers/RequestReplyAnswerSpec.scala
@@ -1,5 +1,7 @@
 package jug.workshops.reactive.patterns.requestreply.answers
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import common.StopSystemAfterAll
@@ -14,7 +16,7 @@ class RequestReplyAnswerSpec extends TestKit(ActorSystem("testingRequestReply"))
 
   "Request-Reply" should {
     "receive responses from simple node" in {
-      val node=system.actorOf(Props[SimpleNode])
+      val node=system.actorOf(Props[SimpleNode],"simpleNode")
 
       node ! Sum((1.0 to 5.0 by 1))
 
@@ -28,6 +30,46 @@ class RequestReplyAnswerSpec extends TestKit(ActorSystem("testingRequestReply"))
         case Response(body) => body mustBe 5.0
       }
 
+    }
+
+    "receive responses from delegate node (how to test async?)" in {
+      import scala.concurrent.duration._
+
+      val node=system.actorOf(Props[DelegateNode],"delegateNode")
+
+      node ! Sum((1.0 to 5.0 by 1))
+      TimeUnit.MILLISECONDS.sleep(100)
+      node ! SquareRoot(25.0)
+      TimeUnit.MILLISECONDS.sleep(100)
+      node ! SquareRoot(36.0)
+
+      expectMsgPF(max = 600 millis){
+        case Response(body) => body mustBe 15.0
+      }
+
+      expectMsgPF(max = 600 millis){
+        case Response(body) => body mustBe 5.0
+      }
+
+      expectMsgPF(max = 600 millis){
+        case Response(body) => body mustBe 6.0
+      }
+
+    }
+
+
+    "receive responses from delegate variant2" in {
+      import scala.concurrent.duration._
+
+      val node=system.actorOf(Props[DelegateNode],"delegateNode2")
+
+      node ! Sum((1.0 to 5.0 by 1))
+      node ! SquareRoot(25.0)
+      node ! SquareRoot(36.0)
+
+      expectMsgAnyOf(max = 600 millis,Response(15.0),Response(5.0),Response(6.0))
+      expectMsgAnyOf(max = 600 millis,Response(15.0),Response(5.0),Response(6.0))
+      expectMsgAnyOf(max = 600 millis,Response(15.0),Response(5.0),Response(6.0))
     }
   }
 
