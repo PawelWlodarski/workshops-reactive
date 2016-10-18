e02fcf8c52387c5023f092b5f2173abdace45e89
pawel <pawel@domena.pl>
2016-10-18 20:58:07 +0200

    [Basics] A1 simple actor

--- src/main/scala/jug/workshops/reactive/akka/a1/exercises/A1SimpleActor1.scala
+++ src/main/scala/jug/workshops/reactive/akka/a1/exercises/A1SimpleActor1.scala
@@ -1,5 +1,6 @@
 package jug.workshops.reactive.akka.a1.exercises
 
+import java.util.Date
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorSystem, Props}
@@ -11,40 +12,33 @@ object A1SimpleActor1 {
 
 
   def main(args: Array[String]) {
+    //what is actorSystem and why we use Props to create actors in factory method instead of simple "new"
     val system=ActorSystem("workshops")
     val actorProperties = Props[SimpleActor]
     val simpleActorInstance = system.actorOf(actorProperties)
 
-
+    //HANDS ON :uncomment first match in 'SimpleActor'
     println("different scala syntax - the same result")
     simpleActorInstance.!("one")  //calling method
     simpleActorInstance !("one")  //without dot
     simpleActorInstance ! "one"  //without simplified
 
-    //exercise : change waitPrint to print and check what will happen
+    //HANDS ON : change waitPrint to print and check what will happen
+    //HANDS ON :uncomment second message
     waitPrint("\nsecond message")
     simpleActorInstance ! "two"
 
+    //HANDS ON : uncomment default
     waitPrint("\nunknown message")
     simpleActorInstance ! "unknown"
 
+    //HANDS ON : ad hoc implementation for different types
     waitPrint("\ndifferent type")
     simpleActorInstance ! 69
+    simpleActorInstance ! new Date()
+    simpleActorInstance ! List(1,2,3,4,5)
+    simpleActorInstance ! (1,2)  //explain tuple for exercises
 
-    //EXERCISES
-//    waitPrint("\nprimary exercise 1 'isEven'")
-//    simpleActorInstance ! 40 //40 is even = true
-//    simpleActorInstance ! 41 //41 is even = false
-//
-//
-//    waitPrint("\nadditional exercise 2 'calculator'")
-//    simpleActorInstance ! (1,2) //result = 3
-//    simpleActorInstance ! (4,3) //result = 7
-//
-//    waitPrint("\nadditional exercise 3 'dedicated calculator'")
-//    lazy val calcActor=???
-//    calcActor ! (1,2) //calc (1+2) = 3
-//    calcActor ! (4,3) //calc (4+3) = 7
 
     system.terminate()
   }
@@ -52,11 +46,12 @@ object A1SimpleActor1 {
 
   class SimpleActor extends Actor{
     override def receive: Receive = {
-      case "one" => println("in actor : received one")
-      case "two" => println("in actor : received two")
-      //EXERCISES
-      //case i:Int =>
-      case msg => println(s"in actor : received unknown message : [value=$msg, type=${msg.getClass} ]")
+//      case "one" => println("in actor : received one")
+//      case "two" =>
+//        println("sending message two to sender")
+//        sender ! "in actor : received two"
+//      case msg => println(s"in actor : received unknown message : [value=$msg, type=${msg.getClass} ]")
+      case _ => println("this catches all messages - uncomment specific cases")
     }
   }
 
