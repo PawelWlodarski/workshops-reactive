b8cb8ebdc448bb1c7c084cc5189048b8851cee08
pawel <pawel@domena.pl>
2016-11-12 22:51:43 +0100

    [Routers] part 1 - fixed class name

--- src/main/scala/jug/workshops/reactive/akka/routing/Part1RoutersCreateDemo.scala
+++ src/main/scala/jug/workshops/reactive/akka/routing/Part1RoutersCreateDemo.scala
@@ -6,7 +6,7 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.routing.{FromConfig, RandomGroup, RoundRobinPool}
 import com.typesafe.config.ConfigFactory
 
-object Part1RoutersCreareDemo {
+object Part1RoutersCreateDemo {
 
   def main(args: Array[String]): Unit = {
     println("*** CREATING ROUTERS ***")
