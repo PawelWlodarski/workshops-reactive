927424c4d2db0786cc9b2ca5a11797f21e0bb84c
pawel <pawel@domena.pl>
2017-07-20 20:02:26 +0200

    [testing] additional conditions

--- src/test/scala/jug/workshops/reactive/testing/TestingActorsDemo.scala
+++ src/test/scala/jug/workshops/reactive/testing/TestingActorsDemo.scala
@@ -1,5 +1,7 @@
 package jug.workshops.reactive.testing
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.testkit.TestActor.AutoPilot
 import akka.testkit.{ImplicitSender, TestActor, TestActorRef, TestKit, TestProbe}
@@ -8,6 +10,8 @@ import common.StopSystemAfterAll
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 import scala.collection.immutable.Seq
+import scala.concurrent.Future
+import scala.concurrent.duration.Duration
 import scala.util.Success
 
 /**
@@ -48,13 +52,15 @@ class TestingActorsDemo extends TestKit(ActorSystem("testing-demo")) with WordSp
       val request = sut ? "getState"
 
       //then
+      // EXPLAIN PATTERN MATCHING
+      // EXPLAIN WHY mapTo works here immediately
       val Success(state) = request.mapTo[Int].value.get
 
       state mustBe 3
     }
   }
 
-  "Synchronous test" should {
+  "Asynchronous test" should {
     "use implicit sender" in {
       val sut = system.actorOf(Props[ActorWithState])
 
@@ -62,8 +68,9 @@ class TestingActorsDemo extends TestKit(ActorSystem("testing-demo")) with WordSp
       sut ! "two"
       sut ! "three"
 
-      val request = sut ! "getState"
+      sut ! "getState"
 
+      //EXPLAIN ASYNC ASSERTION
       expectMsgPF() {
         case state: Int => state mustBe 3
       }
@@ -105,6 +112,17 @@ class TestingActorsDemo extends TestKit(ActorSystem("testing-demo")) with WordSp
       messages must contain only("one","two","three","four")
 
     }
+
+    "expect no messages" in {
+      val probe = TestProbe()
+
+      probe.ref ! "one"
+      probe.ref ! "two"
+      probe.ref ! "three"
+
+      probe.expectMsgAllOf("one","two","three")
+      TestProbe().expectNoMsg(Duration(200,TimeUnit.MILLISECONDS)) // WHEN USEFUL ?
+    }
   }
 
   "Test Probe" should {
@@ -113,13 +131,12 @@ class TestingActorsDemo extends TestKit(ActorSystem("testing-demo")) with WordSp
       val probe1=TestProbe()
       val probe2=TestProbe()
 
-      probe1.setAutoPilot(new AutoPilot {
-        override def run(sender: ActorRef, msg: Any): AutoPilot = {
-          probe2.ref.tell(msg,testActor)
-          TestActor.KeepRunning
-        }
+      probe1.setAutoPilot((_ , msg: Any) => {
+        probe2.ref.tell(msg, testActor)
+        TestActor.KeepRunning
       })
 
+      //SCALA 2.12 !!!!
       probe2.setAutoPilot(new AutoPilot {
         override def run(sender: ActorRef, msg: Any): AutoPilot = {
           sender ! msg
@@ -143,6 +160,46 @@ class TestingActorsDemo extends TestKit(ActorSystem("testing-demo")) with WordSp
       responses must have size(6)
 
     }
+
+    "fish for message" in {
+      val probe1=TestProbe()
+
+      probe1.ref ! "one"
+      probe1.ref ! 22
+      probe1.ref ! "three"
+
+      probe1.ref ! "four"
+      probe1.ref ! 77
+      probe1.ref ! "six"
+
+      val r: Any =probe1.fishForMessage(){
+        case m:Int if m> 50 => true
+        case _ => false
+      }
+
+      r mustBe 77
+    }
+
+    "await for condition" in {
+      var closure = 0
+      import scala.concurrent.ExecutionContext.Implicits.global
+      Future{
+        //Don't do this on production!!!
+        TimeUnit.SECONDS.sleep(1)
+        println("changing closure to 1")
+        closure=1
+      }
+
+      println("start waiting for condition")
+      awaitCond(
+        p = closure>0,
+        max = Duration(2,TimeUnit.SECONDS),
+        interval = Duration(100,TimeUnit.MILLISECONDS))
+
+      println("after condition")
+
+      //awaitAssert
+    }
   }
 }
 
@@ -152,6 +209,6 @@ class ActorWithState extends Actor {
 
   override def receive: Receive = {
     case "getState" => sender ! state
-    case other => state = state + 1
+    case _ => state = state + 1
   }
 }
