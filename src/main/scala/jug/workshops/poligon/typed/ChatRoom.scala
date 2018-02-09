a00325ddbde0e0008491f9c710472427180d6496
PawelWlodarski <p.wlodarski@oberthur.com>
2018-02-09 14:37:25 +0100

    akka typed training

---
+++ src/main/scala/jug/workshops/poligon/typed/ChatRoom.scala
@@ -0,0 +1,82 @@
+package jug.workshops.poligon.typed
+
+import akka.typed.scaladsl.Actor
+import akka.typed.{ActorRef, ActorSystem, Behavior, Terminated}
+
+import scala.concurrent.Await
+
+object ChatRoom {
+
+  sealed trait Command
+
+  final case class GetSession(screenName: String, replyTo: ActorRef[SessionEvent]) extends Command
+
+  private final case class PostSessionMessage(screenName: String, message: String) extends Command
+
+  sealed trait SessionEvent
+
+  final case class SessionGranted(handle: ActorRef[PostMessage]) extends SessionEvent
+
+  final case class SessionDenied(reason: String) extends SessionEvent
+
+  final case class MessagePosted(screenName: String, message: String) extends SessionEvent
+
+  final case class PostMessage(message: String)
+
+  val behavior:Behavior[Command] = chatRoom(List.empty)
+
+  private def chatRoom(sessions: List[ActorRef[SessionEvent]]): Behavior[Command] =
+    Actor.immutable[Command] { (ctx, msg) =>
+      msg match {
+        case GetSession(screenName, client) =>
+          val wrapper = ctx.spawnAdapter {
+            p: PostMessage => PostSessionMessage(screenName, p.message)
+          }
+          client ! SessionGranted(wrapper)
+          chatRoom(client :: sessions)
+        case PostSessionMessage(screenName, message) =>
+          val mp = MessagePosted(screenName,message)
+          sessions foreach (_ ! mp)
+          Actor.same
+      }
+    }
+
+  val gabbler = Actor.immutable[SessionEvent]{ (_,msg) =>
+    msg match {
+      case SessionGranted(handle) =>
+        handle ! PostMessage("Hello World!")
+        Actor.same
+      case MessagePosted(screenName,message) =>
+        println(s"message has been posted by '$screenName': $message")
+        Actor.stopped
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val root: Behavior[String] =Actor.deferred{ ctx =>
+      val chatroom: ActorRef[Command] =ctx.spawn(behavior,"chatroom")
+      val gabblerRef: ActorRef[SessionEvent] =ctx.spawn(gabbler,"gabbler")
+      ctx.watch(gabblerRef)
+
+      Actor.immutablePartial[String]{
+        case (_, "go") =>
+          chatroom ! GetSession("Gabber",gabblerRef)
+          Actor.same
+      }.onSignal{
+        case (_,Terminated(ref)) =>
+          println(s"$ref is terminated")
+          Actor.stopped
+      }
+    }
+
+
+    val system = ActorSystem(root, "chatroom")
+    system ! "go"
+
+    import scala.concurrent.duration._
+    Await.result(system.whenTerminated, 3.seconds)
+
+  }
+
+}
---
+++ src/main/scala/jug/workshops/poligon/typed/CoexistenceTyped2.scala
@@ -0,0 +1,107 @@
+package jug.workshops.poligon.typed
+
+import java.util.concurrent.TimeUnit
+
+import akka.actor.Props
+import akka.typed.scaladsl.Actor
+import akka.typed.{ActorRef, Behavior}
+
+import akka.typed.scaladsl.adapter._
+
+object CoexistenceTyped2 {
+
+
+  def main(args: Array[String]): Unit = {
+    //exampleOne()
+    exampleTwo()
+  }
+
+
+  private def exampleTwo(): Unit ={
+    val system = akka.actor.ActorSystem("TypedWatchingUntyped")
+    val typed: ActorRef[Typed2.Command2] = system.spawn(Typed2.behavior, "Typed")
+    TimeUnit.SECONDS.sleep(3)
+    system.terminate()
+  }
+
+  private def exampleOne() = {
+    val system = akka.actor.ActorSystem("UntypedToTypedSystem")
+    //    val typedSystem: ActorSystem[Nothing] = system.toTyped
+
+    system.actorOf(Props[Untyped], "untypedFirst")
+    TimeUnit.SECONDS.sleep(1)
+    system.terminate()
+  }
+
+  object Typed {
+    sealed trait Command
+    final case class Ping(replyTo: ActorRef[Pong.type]) extends Command
+    case object Pong
+
+
+    val behavior: Behavior[Command] = Actor.immutable{ (ctx, msg) =>
+      msg match {
+        case Ping(replyTo) =>
+          println(s"${ctx.self} got Ping from $replyTo")
+          // replyTo is an untyped actor that has been converted for coexistence
+          replyTo ! Pong
+          Actor.same
+      }
+    }
+  }
+
+  class Untyped extends akka.actor.Actor{
+
+    val second: ActorRef[Typed.Command] =context.spawn(Typed.behavior,"second")
+
+    context.watch(second)
+
+    second ! Typed.Ping(self)
+
+    override def receive: Receive = {
+      case Typed.Pong =>
+        println(s"$self got Pong from ${sender()}")
+        context.stop(second)
+      case akka.actor.Terminated(ref) =>
+        println(s"$self observed termination of $ref")
+        context.stop(self)
+    }
+  }
+
+
+  class Untyped2 extends akka.actor.Actor{
+    override def receive: Receive = {
+      case Typed2.Ping2(replyTo) =>
+        println("received Ping2")
+        replyTo ! Typed2.Pong2
+    }
+  }
+
+  object Typed2 {
+    final case class Ping2(replyTo : ActorRef[Command2])
+    sealed trait Command2
+    case object Pong2 extends Command2
+
+
+
+
+    val behavior:Behavior[Command2] = Actor.deferred{ctx =>
+      val untyped = ctx.actorOf(Props[Untyped2],"second2")
+      ctx.watch(untyped)
+      untyped.tell(Ping2(ctx.self),ctx.self.toUntyped)
+
+      Actor.immutablePartial[Command2]{
+        case (ctx,Pong2) =>
+          println("stoping untyped")
+          ctx.stop(untyped)
+          Actor.same
+      } onSignal {
+        case (_, akka.typed.Terminated(_)) =>
+          println("stopping typed")
+          Actor.stopped
+      }
+    }
+
+  }
+
+}
